8d0a56076a3788692a39191667357a147a4d8dd4
Lukeman ADIO <adiolukeman@gmail.com>
2023-08-29 16:18:56 -0700

    Adding my 1st Scripted files

---
+++ My1stScript.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+echo " Welcome to Bash Scripting "
+echo
+echo " ################################### "
+echo " The Uptime of the System is: "
+uptime
+echo
+echo " ################################### "
+echo " Memory Utilization of the System is: "
+free -m
+echo
+echo " ################################### "
+echo " Disk Utilization of the System is: "
+df -h
+echo
+echo " ################################### "
+
